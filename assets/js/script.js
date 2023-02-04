5fb988c4592fed9eef7a7bf102c151cf7f010966
skouch <skouchy24@gmail.com>
2023-02-03 22:26:22 -0700

    initial commit w/ bootstrap inspired html framework

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// var createTask = function(taskText, taskDate, taskList) {
+//     // create elements that make up a task item
+//     var taskLi = $("<li>").addClass("list-group-item");
+  
+//     var taskSpan = $("<span>")
+//       .addClass("badge badge-primary badge-pill")
+//       .text(taskDate);
+  
+//     var taskP = $("<p>")
+//       .addClass("m-1")
+//       .text(taskText);
+  
+//     // append span and p element to parent li
+//     taskLi.append(taskSpan, taskP);
+  
+  
+//     // append to ul list on the page
+//     $("#list-" + taskList).append(taskLi);
+//   };
