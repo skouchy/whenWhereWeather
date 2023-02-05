b010495e63cc13357d641141594f3e145dc44062
skouch <skouchy24@gmail.com>
2023-02-05 02:38:23 -0700

    add(starter):gathered usable codes for potential use, will apply/restructure when ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+// *FETCH 
+// var getUserRepos = function () {
+//     // fetch("https://api.github.com/users/octocat/repos");
+//     // var response = fetch("https://api.github.com/users/octocat/repos");
+//     // console.log(response);
+  
+//     fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+//       response.json().then(function(data) {
+//         console.log(data);
+//       });
+//     });
+//   }
+//   getUserRepos();
+
+// *creating list items dynamically
 // var createTask = function(taskText, taskDate, taskList) {
 //     // create elements that make up a task item
 //     var taskLi = $("<li>").addClass("list-group-item");
@@ -17,3 +32,51 @@
 //     // append to ul list on the page
 //     $("#list-" + taskList).append(taskLi);
 //   };
+
+//* creating dynamic elements
+// var text = $(this)
+//  .text()
+//  .trim();
+
+//  var textInput = $("<textarea>")
+//   .addClass("form-control")
+//   .val(text);
+
+// $(this).replaceWith(textInput);
+
+//*Getting unknown object inputs
+// tasks[status][index].text = text;
+// saveTasks();
+// // FOR REFERENCE
+// text = "Walk the dog";
+// status = "toDo";
+// index = 0;
+
+//==============================================
+// * LOCALSTORAGE
+
+// var saveTasks = function() {
+//     localStorage.setItem("tasks", JSON.stringify(tasks));
+//   };
+
+//   var loadTasks = function() {
+//     tasks = JSON.parse(localStorage.getItem("tasks"));
+  
+//     // if nothing in localStorage, create a new object to track all task status arrays
+//     if (!tasks) {
+//       tasks = {
+//         inProgress: [],
+//         inReview: [],
+//         done: []
+//       };
+//     }
+//   };
+
+//===============================================
+// * EVENT LISTENER
+
+// document.querySelector("#wrapper").addEventListener("click", function(event) {
+//     if (event.target.matches(".task")) {
+//       console.log("dynamic task was clicked");
+//     }
+//    });
