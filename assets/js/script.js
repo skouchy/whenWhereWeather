d40be53f254fce07f5792d356a5aea18216bf9ae
skouch <skouchy24@gmail.com>
2023-02-12 11:43:04 -0800

    feat(display):title is rendered, for loop is displaying enough dates to add to cards. Idk, we'll call it a slow progress.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,160 +10,143 @@ const apiKey = '8f2f373cdd243b84ebc3cb3c2f92118f';
 let currentWeatherUrl;
 
 
-function formSubmitHandler(event, cityInput) {
-    event.preventDefault();
-    // get value from input element
-    cityInput = locationInputEl.value.trim();
+function displayWeather(cityArray, cityName) {
+    let displayWeatherEl = document.getElementById('current-weather');
 
-    if (cityInput) {
-        //     locationInputEl.value = "";
-        addCity(cityInput);
-        getCurrentWeather(cityInput);
-        console.log(cityInput + " : form handled!")
-        return cityInput;
-    } else {
-        alert("Please enter a valid City");
+    console.log('cityArray');
+    console.log(cityArray);
+
+    let cityTitle = document.createElement('div');
+    let cityHeader = document.createElement('h2');
+    cityHeader.textContent = cityName;
+
+    cityTitle.appendChild(cityHeader);
+    displayWeatherEl.appendChild(cityTitle);
+    
+
+    for (let i = 0; i < cityArray.length; i += 7) {
+        
+            
+        console.log(cityArray[i]);
+        // let dailyWeather = document.createElement('div');
+        // // listItem.textContent = docArray[index].weather;
+        // displayWeatherEl.appendChild(dailyWeather);
+        // console.log(forecastResponse[index]);
     }
-};
 
+    // fetch(getCurrentWeather)
+    // .then(function(response){
+    //     //convert to JSON object
+    //     return response.json();
+    // })
+    // .then(function(data) {
+    //     //display in HTML here
+
+}
+// let saveCities = function() {
+//     localStorage.setItem("cities", JSON.stringify(cities));
+//   };
+// function forecastResponse(cityArray) {
+//     cityArray = JSON.parse(localStorage.getItem("cities"));
+// displayWeather(forecastResponse);
 
 // *FETCH 
-function getForecast(lat, lon) {
+function getForecast(lat, lon, cityName) {
     let fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
-    console.log('lat : ' + lat,'lon : ' + lon);
-    fetch(fiveDayUrl).then(function (response) {
-        console.log(response);
-        response.json().then(function (data) {
-            console.log(data);
+    console.log('lat : ' + lat, 'lon : ' + lon);
+    fetch(fiveDayUrl)
+        .then(function (response) {
+            if (!response.ok) {
+                alert('dis sum 5day bullshit');
+                throw response.json();
+            }
+
+            console.log(response);
+            return response.json();
         })
-    })
+
+        .then(function (data) {
+            let cityArray = data.list;
+            console.log(cityArray);
+            displayWeather(cityArray, cityName);
+            return cityArray;
+        })
 }
+// function forecastResponse(cityArray) {
+//     let forecastResponse = cityArray.toString;
+//     console.log('forecastResponse from this other one');
+//     console.log(forecastResponse);
+//     // displayWeather(forecastResponse);
+// }
 
-// function getCurrentWeather(cityInput) {
-//     console.log(cityInput);
-// creating list items dynamically
 function addCity(cityInput) {
-    console.log(cityInput);
-    // getCurrentWeather();
     let listCity = $("<li>")
-        .addClass('m-1')
+        .addClass('list-group-item list-group-item-action list-group-item-secondary')
         .text(cityInput);
-    let cityWeatherLink = $('<a>').attr('href', currentWeatherUrl);
-
-    listCity.addClass("list-group-item list-group-item-action list-group-item-secondary");
-
-        
-
-    listCity.append(cityWeatherLink);
+    // let cityWeatherLink = $('<a>').attr('href', currentWeatherUrl);
 
     // append new city to div#city-list in html
     $("#city-list").append(listCity);
-    // return cityInput;
 };
 
 function getCurrentWeather(cityInput, lat, lon) {
     let currentWeatherUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&appid=" + apiKey;
     fetch(currentWeatherUrl)
         .then(function (response) {
-            console.log(response);
-            if (response.ok) {
-                response.json().then(function (data) {
-                    lat = data[0].lat;
-                    lon = data[0].lon;
-                    
-                    // console.log(lat);
-                    // console.log(lon);
-                    getForecast(lat, lon);
-                    return currentWeatherUrl;
-                    // for (let i = 0; i < data.length; i++) {
-                    //     //create a list element
-                    //     let cityListItem = $('<li>');
-                    //     cityListItem.textContent = data[i].html_url;
-                        // .attr('href', getCurrentWeather);
-
-                        // cityListItem.addClass("list-group-item list-group-item-action list-group-item-secondary");
-
-                        // let listCity = $("<span>")
-                        // .addClass('m-1')
-                        // .text(cityInput);
-
-                        // cityListItem.append(listCity);
-
-                        // // append new city to div#city-list in html
-                        // cityListEl.append(cityListItem);
-                        // // return cityInput;
-                    })
-                
-            } else {
+            if (!response.ok) {
                 alert('dis sum bullshit');
+                throw response.json();
             }
+            return response.json();
+        })
+
+        .then(function (data) {
+            cityName = data[0].name + ", " + data[0].state;
+            lat = data[0].lat;
+            lon = data[0].lon;
+
+            console.log(cityName);
+            // console.log(lon);
+            getForecast(lat, lon, cityName);
+            // getCurrentWeather(cityName);
+            // return currentWeatherUrl;
+            // for (let i = 0; i < data.length; i++) {
+            //     //create a list element
+            //     let cityListItem = $('<li>');
+            //     cityListItem.textContent = data[i].html_url;
+            // .attr('href', getCurrentWeather);
+
+            // cityListItem.addClass("list-group-item list-group-item-action list-group-item-secondary");
+
+            // let listCity = $("<span>")
+            // .addClass('m-1')
+            // .text(cityInput);
+
+            // cityListItem.append(listCity);
+
+            // // append new city to div#city-list in html
+            // cityListEl.append(cityListItem);
+            // // return cityInput;
         })
 };
-// response.json().then(function (data) {
-//     console.log(data.response);
-// })
-//     })
-// }
-// **let getCurrentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey;
-// let displayWeatherEl = document.getElementById('render-weather');
 
-// fetch(getCurrentWeather)
-// .then(function(response){
-//     //convert to JSON object
-//     return response.json();
-// })
-// .then(function(data) {
-//     //display in HTML here
-//     let cityArray = data.response.city;
-//     for (let index = 0; index < cityArray.length; index++) {
-//         let listItem = document.createElement('li');
-//         listItem.textContent = docArray[index].weather;
-//         listEl.appendChild(listItem);
-//     }
-// })
-// .catch(function(error){
-//     //in case there is an error
-//     console.log(error);
+function formSubmitHandler(event, cityInput) {
+    event.preventDefault();
+    // get value from input element
+    cityInput = locationInputEl.value.trim();
+
+    if (!cityInput) {
+        alert("Please enter a valid City");
+        return;
+    }
+    //     locationInputEl.value = "";
+    addCity(cityInput);
+    getCurrentWeather(cityInput);
+    console.log(cityInput + " : form handled!");
+}
+
 //** */ });
 
-
-// function statusErr(badRequestUrl) {
-//     fetch(badRequestUrl).then(function (response) {
-//         if (response.status !== 200) {
-//             // document.location.replace('./404.html')
-//             console.log(response.status)
-//         } else {
-//             return response.json();
-
-//         }
-//     });
-// }
-// function getCityWeather(cityInput) {
-//     let geoCodeUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-//     fetch(geoCodeUrl).then(function(response){
-//     response.json().then(function(data){
-//         console.log(data);
-//     })    
-// });
-// }
-
-//     });   
-// }
-
-//ORRRRR
-
-//     // fetch("https://api.github.com/users/octocat/repos");
-//     // let response = fetch("https://api.github.com/users/octocat/repos");
-//     // console.log(response);
-
-//     fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-//       response.json().then(function(data) {
-//         console.log(data);
-//       });
-//     });
-//   }
-//   citySearchFormEl.addEventListener("submit", formSubmitHandler);
 
 //   <li class="list-group-item">An item</li>
 /* <span class="badge bg-secondary">New</span> */
@@ -181,37 +164,16 @@ function getCurrentWeather(cityInput, lat, lon) {
 
 // $(this).replaceWith(textInput);
 
-//*Getting unknown object inputs
-// tasks[status][index].text = text;
-// saveTasks();
-// // FOR REFERENCE
-// text = "Walk the dog";
-// status = "toDo";
-// index = 0;
 
 //==============================================
 // * LOCALSTORAGE
 
-// let saveCities = function() {
-//     localStorage.setItem("cities", JSON.stringify(cities));
-//   };
 
-//   let loadCities = function() {
-//     cities = JSON.parse(localStorage.getItem("cities"));
 
-//     // if nothing in localStorage, create a new object to track all task status arrays
-//     if (!cities) {
-//       cities = {
-//         inProgress: [],
-//         inReview: [],
-//         done: []
-//       };
-//     }
-//   };
 
 //===============================================
 // * EVENT LISTENER
 
 // render(locationInputEl.attr(placeholder));
 citySearchBtn.addEventListener("click", formSubmitHandler);
-
+// listCity.addEventListener('click', currentWeatherUrl)
